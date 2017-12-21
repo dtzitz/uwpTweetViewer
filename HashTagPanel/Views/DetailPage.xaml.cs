a5c473184b7e676a1eac00327fc8a54c43bdf5e5
Dylan Z <dtzitz@live.com>
2017-12-21 11:17:52 -0500

    Add project files.

---
+++ HashTagPanel/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using HashTagPanel.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace HashTagPanel.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
